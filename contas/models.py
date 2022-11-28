bb16565913b50bd4ea8fe42b70cb441dbdbd8dbb
Gabriel Pereira <ggabriel.p2003@gmail.com>
2022-11-27 23:04:34 -0300

    feat/tests

--- contas/models.py
+++ contas/models.py
@@ -6,7 +6,7 @@ class Conta(models.Model):
     Tipo = models.IntegerField()
     Data = models.DateField()
     Valor = models.IntegerField()
-    CPF = models.CharField(max_length=50)
+    CPF = models.CharField(max_length=14)
     Cartão = models.CharField(max_length=12)
     Hora = models.TimeField()
     DonoDaLoja = models.CharField(max_length=50)
--- contas/urls.py
+++ contas/urls.py
@@ -2,7 +2,7 @@ from contas import views
 from django.urls import path
 
 urlpatterns = [
-    path("inicio/", views.inicio),
+    path("start/", views.start),
     path("uploadFile/", views.UploadedFileViews.as_view(), name="processando_form"),
     path("ShowData/", views.ShowResult),
     path("dados/", views.GetDataViews.as_view()),
